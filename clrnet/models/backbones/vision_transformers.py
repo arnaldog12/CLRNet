73beb46add93560b54bdf7f838f793aca5d6ac0d
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2024-02-12 11:08:51 -0300

    feat: load pretrained weights

--- clrnet/models/backbones/vision_transformers.py
+++ clrnet/models/backbones/vision_transformers.py
@@ -1,5 +1,9 @@
 from torch import nn
-from torchvision.models.vision_transformer import VisionTransformer, vit_b_16
+from torchvision.models.vision_transformer import (
+    VisionTransformer,
+    ViT_B_16_Weights,
+    vit_b_16,
+)
 
 from clrnet.models.registry import BACKBONES
 
@@ -9,7 +13,7 @@ class VisionTransformerWrapper(nn.Module):
     def __init__(self, cfg=None):
         super(VisionTransformerWrapper, self).__init__()
         self.cfg = cfg
-        self.model = vit_b_16()
+        self.model = vit_b_16(weights=ViT_B_16_Weights.IMAGENET1K_V1)
 
     def forward(self, x):
         return self.model(x)
