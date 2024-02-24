709c9eb7231b002015cdf8c40b3d0062d7f3095d
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2024-02-24 14:03:57 -0300

    feat: export script

---
+++ export.py
@@ -0,0 +1,17 @@
+import torch
+
+from clrnet.models.registry import build_net
+from clrnet.utils.config import Config
+from clrnet.utils.net_utils import load_network
+
+config_path = "configs/clrnet/clr_resnet18_culane.py"
+model_path = "C:/Users/arnal/Downloads/ResNet18_CUlane.pth"
+
+cfg = Config.fromfile(config_path)
+
+net = build_net(cfg)
+load_network(net, model_path)
+
+torch_input = torch.randn(1, 3, cfg.img_h, cfg.img_w)
+onnx_program = torch.onnx.dynamo_export(net, torch_input)
+onnx_program.save("my_image_classifier.onnx")
