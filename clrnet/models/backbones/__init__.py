6054a245c6c467a30d58049117798e29dda03c9d
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2024-02-26 21:46:45 -0300

    feat: add fspnet

--- clrnet/models/backbones/__init__.py
+++ clrnet/models/backbones/__init__.py
@@ -1,3 +1,4 @@
 from .dla34 import DLA
+from .fspnet.FSPNet_model import FSPNet
 from .resnet import ResNet
 from .vision_transformers import VisionTransformer
---
+++ clrnet/models/backbones/fspnet/fspnet_wrapper.py
@@ -0,0 +1,15 @@
+from torch import nn
+
+from clrnet.models.backbones.fspnet.FSPNet_model import FSPNet
+from clrnet.models.registry import BACKBONES
+
+
+@BACKBONES.register_module
+class FSPNetWrapper(nn.Module):
+    def __init__(self, cfg=None):
+        super(FSPNetWrapper, self).__init__()
+        self.cfg = cfg
+        self.model = FSPNet(cfg.ckpt, cfg.img_h)
+
+    def forward(self, x):
+        return [self.model.forward(x)]
